13255e85e06521c28eee2f416278c95bcc4ea628
James Wyatt Cready <wyatt.cready@lanetix.com>
2016-06-17 20:16:02 -0400

    Allow any subdomains of lanetix.com to make CORS requests against the service

--- middleware/cors.js
+++ middleware/cors.js
@@ -5,16 +5,22 @@ var cors = require('cors'),
     'X-Total-Count'
   ],
   corsConfig = {
+    origin: /\.lanetix\.com$/i,
     credentials: true,
     maxAge: 10 * 60 // this is the maximum we can cache in chrome
   };
 
 module.exports = function (app) {
   var options = app.get('options');
+  var corsOrigin = options.corsOrigin;
 
   // specify allowed origins
-  if (app.get('options').corsOrigin) {
-    corsConfig.origin = options.corsOrigin;
+  if (corsOrigin) {
+    if (Array.isArray(corsOrigin)) {
+      corsConfig.origin = corsOrigin.concat([corsConfig.origin]); // don't mutate
+    } else if (typeof corsOrigin !== 'string' || !corsConfig.origin.test(corsOrigin)) {
+      corsConfig.origin = [corsOrigin, corsConfig.origin];
+    }
   }
 
   // specify allowed headers

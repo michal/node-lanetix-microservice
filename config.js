c3be0a72b111e8e4ddc479cdaf2782863733038f
Troy Goode <troygoode@gmail.com>
2017-02-16 12:03:53 -0800

    add support for "development" in NODE_ENV

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 var keys = require('./keys');
 
 module.exports = {
-  jwtPublicKey: process.env.JWT_PUBLIC_KEY || keys[process.env.NODE_ENV || 'test'].publicKey,
+  jwtPublicKey: process.env.JWT_PUBLIC_KEY || keys[(!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 'test' : process.env.NODE_ENV].publicKey,
   corsOrigin: process.env.CORS_ORIGIN,
   isProduction: process.env.NODE_ENV === 'production'
 };
